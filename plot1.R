3859ef01cb9b179063a60118a8cbb63ce2058936
Valerio <valerio.pa@gmail.com>
2015-04-12 19:08:58 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,4 +3,9 @@ dataF <- data
 dataF$Date <- as.Date(dataF$Date,format="%d/%m/%Y")
 dataF2  <- dataF[dataF$Date == as.Date("2007-02-01") | dataF$Date ==  as.Date("2007-02-02"),]
 dataF2$Global_active_power <- as.numeric(as.character(dataF2$Global_active_power))
-hist(dataF2$Global_active_power, breaks=12, col="red")
+hist(dataF2$Global_active_power, breaks=12, 
+     col="red", main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)",
+     cex.axis=0.6, cex.lab=0.6, cex.main=0.7)
+dev.copy(png,"plot1.png",width=480,height=480,units="px",res=100)
+dev.off()
